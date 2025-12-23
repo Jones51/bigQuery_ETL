fe6156c9afa1011c1a9c8328bef6b6040c07c813
Jones51 <joaomarcosa51@gmail.com>
2025-12-23 12:15:32 -0300

    "Atualizado projeto principal com controle de memória e adicionado classes"

--- project/scripts/etl_project.py
+++ project/scripts/etl_project.py
@@ -9,7 +9,143 @@ from sqlalchemy import create_engine
 import pymongo
 from pymongo import MongoClient
 from google.cloud import bigquery
+import gc
+import psutil
+import time
 
+# ==============================================================
+# Utilitários auxiliares
+# ==============================================================
+class MemoryManager():
+    """
+    Classe para gerenciamento de memória.
+    
+    Métodos:
+        - get_memory: verifica a quantidade de memória.
+        - clean_memory: limpa a memória.
+        - wait_memory: espera até que a memória esteja abaixo do limite definido.
+    """
+    @staticmethod
+    def get_memory():
+        try:
+            return psutil.virtual_memory().percent
+        except Exception as e:
+            print(f"Erro ao obter uso de memória: {e}")
+            return None
+        
+    @staticmethod
+    def clean_memory(max_percent = 90):
+        usage = MemoryManager.get_memory()
+        
+        if usage is None:
+            return False
+        
+        if usage > max_percent:
+            gc.collect()
+            time.sleep(0.5)
+            print("Memória limpa.")
+
+    @staticmethod
+    def wait_memory(max_percent = 90, max_wait_minutes = 5):
+        start_wait = time.time()
+        max_time_seconds = max_wait_minutes * 60
+        
+        while True:
+            usage = MemoryManager.get_memory()
+            if usage is None:
+                return True
+            
+            if usage <= max_percent:
+                return True
+            
+            elapsed_time = (time.time() - start_wait)
+            if elapsed_time >= max_time_seconds:
+                print("Tempo máximo de espera atingido.")
+                return True
+            
+            print(f"Uso de memória alto ({usage}%). Aguardando...")
+            MemoryManager.clean_memory(max_percent)
+            time.sleep(5)
+        
+class RobustAPI():
+    """
+    Classe para chamadas robustas à API com tentativas e tratamento de erros.
+    """
+    
+    def __init__(self, url, headers):
+        self.url = url.strip('/')
+        self.headers = headers
+        
+    def makeRequest(self, endpoint, params=None, max_retries=5):
+        attempt = 0
+        while attempt < max_retries:
+            try:
+                full_url = f"{self.url}/{endpoint.lstrip('/')}"
+                response = requests.get(full_url, headers=self.headers, params=params)
+                if response.status_code == 200:
+                    return response.json()
+                elif response.status_code in [429, 500, 502, 503, 504]:
+                    print(f"Tentativa {attempt+1}/{max_retries} - Erro temporário ({response.status_code}). Tentando novamente...")
+                    attempt += 1
+                    time.sleep(2 ** attempt)
+                else:
+                    print(f"Erro ao chamar API: {response.status_code} - {response.text}")
+                    response.raise_for_status()
+            except requests.RequestException as e:
+                print(f"Exceção durante a chamada à API: {e}")
+                attempt += 1
+                time.sleep(2 ** attempt)
+
+# ==============================================================
+# Classe APIs ETL
+# ==============================================================
+class UserData(RobustAPI):
+    """
+    Classe para processo de extract e transform da API de usuários.
+    """
+    def __init__(self, api_url):
+       #Controle de memória
+       MemoryManager.wait_memory(max_percent=90, max_wait_minutes=5) 
+        
+       self.api_url = api_url
+       self.client = RobustAPI(api_url, headers={})
+       
+       MemoryManager.clean_memory(max_percent=90)
+       
+    def fetch_data(self):
+        """
+        Extrai os dados da API.
+        """
+        return self.client.makeRequest(endpoint='', params={})
+    
+    def data_transform(self, data):
+        """
+        Transforma os dados extraídos da API em um DataFrame e faz tratamentos necessários.
+        
+        Args:
+            data (dict): Dados extraídos da API.
+        Returns:
+            pd.DataFrame: DataFrame transformado.
+        """
+        data_df = pd.json_normalize(data['results'])
+
+        # Tratando o nome das colunas
+        data_df.columns = (
+            data_df.columns
+            .str.replace('.', '_', regex=False)
+            .str.lower()
+            .str.strip()
+        )
+
+        # Convertendo os valores numéricos para string
+        numeric_columns = data_df.select_dtypes(include=['int64', 'float64']).columns
+        data_df[numeric_columns] = data_df[numeric_columns].astype(str)
+
+        return data_df
+
+# ==============================================================
+# Classes de Loaders nos Bancos
+# ==============================================================
 def load_config(env_path: Optional[str] = None) -> Dict[str, str]:
     """
     Load environment variables from a .env file and return them as a dictionary.
@@ -43,46 +179,6 @@ def load_config(env_path: Optional[str] = None) -> Dict[str, str]:
         "BIG_QUERY_PROJECT_ID" : os.getenv("BIG_QUERY_PROJECT_ID"),
     }
 
-def api_etl():
-    """
-    Extract data from a public API, transform it, and load it into PostgreSQL, MongoDB, and BigQuery.
-    """
-    try:
-        response = requests.get('https://randomuser.me/api/')
-        response.raise_for_status()
-        data = response.json()
-    except requests.RequestException as e:
-        print(f"Error fetching data from API: {e}")
-        data = {"results": []}
-
-    return data
-
-def data_transformation(data):
-    """
-    Transform the raw data from the API into a pandas DataFrame.
-
-    Args:
-        data (dict): Raw data from the API.
-
-    Returns:
-        pd.DataFrame: Transformed DataFrame.
-    """
-    data_df = pd.json_normalize(data['results'])
-
-    # Tratando o nome das colunas
-    data_df.columns = (
-        data_df.columns
-        .str.replace('.', '_', regex=False)
-        .str.lower()
-        .str.strip()
-    )
-
-    # Convertendo os valores numéricos para string
-    numeric_columns = data_df.select_dtypes(include=['int64', 'float64']).columns
-    data_df[numeric_columns] = data_df[numeric_columns].astype(str)
-
-    return data_df
-
 def postgresql_load(data_df, config):
     """
     Load the DataFrame into a PostgreSQL database.
@@ -102,7 +198,6 @@ def postgresql_load(data_df, config):
 
     colunas_relacionais = ['email','name_title', 'name_first', 'name_last', 'location_street_number', 'location_street_name','location_city', 'location_state', 'location_country', 'location_postcode']
     data_relacionais = data_df[colunas_relacionais].copy()
-    data_relacionais.head(5)
 
     data_relacionais.to_sql(
         name='users', 
@@ -168,11 +263,15 @@ def bigquery_load(data_df, config):
 
 if __name__ == "__main__":
 
-    if data := api_etl():
+    #Inicializando 
+    api_url = 'https://randomuser.me/api/'
+    user_data_api = UserData(api_url)
+    
+    if data := user_data_api.makeRequest(endpoint=''):
         print("Dados obtidos com sucesso da API.")
         
         # Transformação dos dados
-        data_df = data_transformation(data)
+        data_df = user_data_api.data_transform(data)
         print("Dados transformados com sucesso.")
 
         # Carregando as configurações do .env
